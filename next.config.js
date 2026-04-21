1d907bcf7e853db9cf9e79680314226af31d235b
Vinciarya <kosliaaryan@gmail.com>
2026-04-21 22:34:25 +0530

    chore: suppress non-error infrastructure logs in webpack configuration

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
     ],
   },
   webpack: (webpackConfig, { webpack }) => {
+    webpackConfig.infrastructureLogging = {
+      level: 'error',
+    };
+
     webpackConfig.plugins.push(
       // Remove node: from import specifiers, because Next.js does not yet support node: scheme
       // https://github.com/vercel/next.js/issues/28774
